f019e93e29012cdd7b6544a19dd3a2b93f18dff3
Kallayan Datta <kallayandatta81@@example.com>
2025-07-12 00:18:21 +0600

    Quick Sort added

---
+++ QuickSort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int l, int h)
+{
+    int pivot = arr[l];
+    int i = l + 1;
+    int j = h;
+
+    while (i <= j)
+    {
+        while (i <= h && arr[i] <= pivot)
+        {
+            i++;
+        }
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[l], arr[j]);
+    return j;
+}
+
+void quickSort(int arr[], int l, int h)
+{
+    if (l < h)
+    {
+        int p = partition(arr, l, h);
+        quickSort(arr, l, p - 1);
+        quickSort(arr, p + 1, h);
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {8, 9, 2, 4, 5, 1, 11, 7, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
